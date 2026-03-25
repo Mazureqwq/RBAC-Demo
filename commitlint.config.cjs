15be705c30e286221b18f707f609dc06fac78a67
Mazure <10556213+mazure0215@user.noreply.gitee.com>
2026-03-25 15:33:17 +0800

    feat: 测试拦截

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,21 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "perf",
+        "revert",
+      ],
+    ],
+    "subject-empty": [2, "never"],
+  },
 };
